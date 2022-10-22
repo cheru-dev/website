e24ed47ef6cab713adb9049be02ae6e3b76a88c4
Cheru Berhanu <mail@cheru.dev>
2022-10-21 21:59:13 -0400

    fix email in header

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -5,7 +5,7 @@
 <div class="header {$theme}">
     <p class="title"><a href="/"><b>Cheru.dev</b></a></p>
     <div class="links">
-        <p><a href="mailto:hi@cheru.dev"><b>E-Mail</b></a></p>
+        <p><a href="mailto:cheru@cheru.dev"><b>Email</b></a></p>
         <p class="link" style="padding-right: 0; padding-left: 0"><b>|</b></p>
         <p class="link" on:click={() => {
                     $theme = $theme === 'dark' ? 'light' : 'dark'
